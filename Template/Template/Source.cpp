c59be9e51ac2f7e24b912bf1e4665accecef2c0d
ceej_ <ceej_7@hotmail.com>
2019-03-08 14:54:23 +0800

    templated modified

--- Template/Template/Source.cpp
+++ Template/Template/Source.cpp
@@ -5,48 +5,30 @@
 #include <stdlib.h>
 #include <math.h>
 #include <algorithm>
-#include <string.h>
 #include <limits.h>
+#include <string>
 
-using namespace std;
+using namespace::std;
 
-long long dpArray[2020];
-int s[2020];
-int  len[2020];
-int n, m;
+char word[20];
+
+char tab[105][105];
+int down[105][105];
+int rt[105][105];
+int rd[105][105];
+struct _THUNDER {
+	int iVersion;
+	char cTag;
+	char cAdv;
+	int iUser;
+	char cEnd;
+}Thunder;
 
 int main()
 {
-	while (scanf("%d%d", &n, &m) != EOF)
-	{
-		int counter = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			dpArray[i] = LLONG_MAX;
-			scanf("%d", &len[i]);
-			counter += len[i];
-			s[i] = counter;
-		}
-		dpArray[0] = 0;
-		s[0] = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = i; j >= 1; j--)
-			{
-				long long res = m - i + j;
-				res =res- (long long)(s[i]-s[j-1]);
-				if (res < 0)
-					continue;
-				else {
-					long long res2 = res * res*res;
-					if (i == n) res2 = 0;
-					if (dpArray[j-1]!=LLONG_MAX&&res2 + dpArray[j - 1] <= dpArray[i])
-					{
-						dpArray[i] = res2 + dpArray[j - 1];
-					}
-				}
-			}
-		}
-		printf("%lld\n", dpArray[n]);
-	}
+	
+	int a[5] = { 1,2,3,4,5 };
+	int *ptr = (int *)(&a + 1);
+	cout <<a<< &a << endl;
+	system("pause");
 }
